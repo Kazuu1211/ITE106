8abab1d36492d731d2f60f3774918435dd6e662a
andrie1211 <andrielerum@gmail.com>
2024-10-15 14:43:03 +0800

    Main.java

--- 02TextFileManipulationScannerLoop/Main.java
+++ 02TextFileManipulationScannerLoop/Main.java
@@ -1,1 +1,13 @@
+import java.util.Scanner; 
+import java.io.File;
 
+public class Main
+{
+    public static void main(String[] args) throws Exception 
+    	File mamamoFile = new File("Kazuu1211File.txt");
+        Scanner sino = new Scanner(mamamoFile);
+  
+        while(omsim.hasNextLine())    	
+        	System.out.println(omsim.nextLine());
+    }
+}
