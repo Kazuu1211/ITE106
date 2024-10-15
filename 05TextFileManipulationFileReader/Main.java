f945bd4cfe0eebd105cd748c32b081b07f469896
andrie1211 <andrielerum@gmail.com>
2024-10-15 14:57:33 +0800

    Update Main.java

--- 05TextFileManipulationFileReader/Main.java
+++ 05TextFileManipulationFileReader/Main.java
@@ -1,1 +1,16 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File mamamoFile = new File("Kazuu1211File.txt");
+        FileReader fr = new FileReader(mamamoFile);
+
+        int i;
+        while ((i = fr.read()) != -1) {
+            System.out.print((char) i);
+        }
+    }
+}
