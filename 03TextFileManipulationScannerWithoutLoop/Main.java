0b96dfe6501a9d22d6e8a6dc19291df6fdff6f7a
andrie1211 <andrielerum@gmail.com>
2024-10-15 15:02:35 +0800

    Update Main.java

--- 03TextFileManipulationScannerWithoutLoop/Main.java
+++ 03TextFileManipulationScannerWithoutLoop/Main.java
@@ -1,1 +1,13 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File mamamofile = new File("Kazuu1211File.txt");
+        Scanner sc = new Scanner(mamamofile);
+     
+        sc.useDelimiter("\\Z");
+        System.out.println(sc.next());
+    }
+}
