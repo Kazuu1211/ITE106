24b8833fe0739415f73ab2c2ecf29d7cce3b1a6b
andrie1211 <andrielerum@gmail.com>
2024-10-19 13:17:59 +0800

    Update Main.java

--- 04TextFileManipulationBufferedReader/Main.java
+++ 04TextFileManipulationBufferedReader/Main.java
@@ -1,1 +1,12 @@
+import java.io.*;
 
+public class BufferedReaderTextFile {
+    public static void main(String[] args) throws Exception {
+		File mamamoFile = new File("Kazuu1211File.txt");
+        BufferedReader br = new BufferedReader(new FileReader(mamamoFile));
+        String st;
+        while((st = br.readLine()) != null) {
+            System.out.println(st);
+        }
+    }
+}
